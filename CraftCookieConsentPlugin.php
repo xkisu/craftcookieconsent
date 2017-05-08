b335c17e7cebc92bce5b541203a6da0063495224
Keith Mitchell <keith.mitchell@live.ca>
2017-05-08 13:38:53 -0700

    Initial Commit

---
+++ CraftCookieConsentPlugin.php
@@ -0,0 +1,88 @@
+<?php
+namespace Craft;
+
+class CraftCookieConsentPlugin extends BasePlugin
+{
+    function getName()
+    {
+        return Craft::t('Cookie Consent');
+    }
+
+    function getVersion()
+    {
+        return '1.0';
+    }
+
+    function getDeveloper()
+    {
+        return 'Kisu';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'http://keithcod.es';
+    }
+
+    protected function defineSettings(){
+        return array(
+            'position' => array(AttributeType::String, 'default' => 'bottom'),
+            'layout' => array(AttributeType::String, 'default' => 'block'),
+            'palette' => array(AttributeType::String, 'default' => 'default'),
+            'palette_banner' => array(AttributeType::String, 'default' => '#000000'),
+            'palette_button' => array(AttributeType::String, 'default' => '#f1d600'),
+            'palette_banner_text' => array(AttributeType::String, 'default' => '#ffffff'),
+            'palette_button_text' => array(AttributeType::String, 'default' => '#000000'),
+            'learn_more_link' => array(AttributeType::String, 'default' => 'http://cookiesandyou.com/'),
+            'message' => array(AttributeType::String, 'default' => 'This website uses cookies to ensure you get the best experience on our website.'),
+            'dismiss' => array(AttributeType::String, 'default' => 'Got it!'),
+            'learn' => array(AttributeType::String, 'default' => 'Learn More'),
+
+
+        );
+    }
+
+    public function getSettingsHtml() {
+       return craft()->templates->render('craftcookieconsent/_settings', array(
+           'settings' => $this->getSettings()
+       ));
+    }
+
+    function init() {
+
+        //if ( !craft()->request->isCpRequest() ) { // we need it to run on the settings page for the plugin as well
+            craft()->templates->includeCssResource('craftcookieconsent/cookieconsent.min.css');
+            craft()->templates->includeJsResource('craftcookieconsent/cookieconsent.min.js', false);
+            if ( !craft()->request->isCpRequest() ) {
+                $settings = craft()->plugins->getPlugin('craftcookieconsent')->getSettings();
+                $script = '
+                  <script>
+                    window.addEventListener("load", function(){
+                    window.cookieconsent.initialise({
+                        "palette": {
+                          "popup": {
+                            "background": "'. $settings->palette_banner .'",
+                            "text": "'. $settings->palette_banner_text .'"
+                          },
+                          "button": {
+                              "background":  "'. $settings->layout .'" == "wire" ? "transparent" :  "'. $settings->palette_button .'",
+                              "text": "'. $settings->layout .'" == "wire" ? "'. $settings->palette_button .'" : "'. $settings->palette_button_text .'",
+                              "border":  "'. $settings->layout .'" == "wire" ? "'. $settings->palette_button .'" : undefined
+                          }
+                      },
+                      "position": "'. $settings->position .'" == "toppush" ? "top" : "'. $settings->position .'",
+                      "static": "'. $settings->position .'" == "toppush",
+                      "theme": "'. $settings->layout .'",
+                      "content": {
+                          "message": "'. $settings->message .'",
+                          "dismiss": "'. $settings->dismiss .'",
+                          "link": "'. $settings->learn .'",
+                          "href": "'. $settings->learn_more_link .'"
+                      }
+                    })});
+                  </script>
+                ';
+                craft()->templates->includeFootHtml($script, $first = false);
+            }
+        //}
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Cookie Consent plugin for Craft CMS
+
+Adds a cookie consent popup the complies with the cookie consent laws.
+
+Uses the ![Cookie Consent](https://cookieconsent.insites.com/download/) JavasScript library to show the popup.
+
+## Installation
+
+1. Download & unzip the repository and place the `craftcookieconsent` directory into your `craft/plugins` directory
+2.  -OR- do a `git clone https://github.com/xkisu/craftcookieconsent.git` directly into your `craft/plugins` folder.  You can then update it with `git pull`
+3.  -OR- install with Composer via `composer require xkisu/craftcookieconsent`
+4. Install plugin in the Craft Control Panel under Settings > Plugins
+5. The plugin folder should be named `craftcookieconsent` for Craft to see it.  GitHub recently started appending `-master` (the branch name) to the name of the folder for zip file downloads.
+
+Craft Cookie Consent works on Craft 2.4.x and Craft 2.5.x.
+
+## Configuring Cookie Consent
+
+Either go to the plugin's install page and click on the settings cog next to the plugin entry, or go to the main settings page and click on `Cookie Consent` under the plugins sub-section.
+
+As you update the settings a live preview of the cookie popup will change to show what it will look like to the end user. 
