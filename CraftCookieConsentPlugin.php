d0dda5d8002f11f3a74f222ad950b39116d89a5e
Max <max@maxkorlaar.com>
2017-10-01 14:43:52 +0200

    Update code style :)

--- CraftCookieConsentPlugin.php
+++ CraftCookieConsentPlugin.php
@@ -1,50 +1,72 @@
 <?php
 namespace Craft;
 
+/**
+ * Class CraftCookieConsentPlugin
+ *
+ * @package Craft
+ */
 class CraftCookieConsentPlugin extends BasePlugin
 {
+    /**
+     * @return string
+     */
     function getName()
     {
         return Craft::t('Cookie Consent');
     }
 
+    /**
+     * @return string
+     */
     function getVersion()
     {
         return '1.0.1';
     }
 
+    /**
+     * @return string
+     */
     function getDeveloper()
     {
         return 'Kisu';
     }
 
+    /**
+     * @return string
+     */
     function getDeveloperUrl()
     {
         return 'http://keithcod.es';
     }
 
+    /**
+     * @return array
+     */
     protected function defineSettings(){
-        return array(
-            'position' => array(AttributeType::String, 'default' => 'bottom'),
-            'layout' => array(AttributeType::String, 'default' => 'block'),
-            'palette' => array(AttributeType::String, 'default' => 'default'),
-            'palette_banner' => array(AttributeType::String, 'default' => '#000000'),
-            'palette_button' => array(AttributeType::String, 'default' => '#f1d600'),
-            'palette_banner_text' => array(AttributeType::String, 'default' => '#ffffff'),
-            'palette_button_text' => array(AttributeType::String, 'default' => '#000000'),
-            'learn_more_link' => array(AttributeType::String, 'default' => 'http://cookiesandyou.com/'),
-            'message' => array(AttributeType::String, 'default' => 'This website uses cookies to ensure you get the best experience on our website.'),
-            'dismiss' => array(AttributeType::String, 'default' => 'Got it!'),
-            'learn' => array(AttributeType::String, 'default' => 'Learn More'),
+        return [
+            'position' => [AttributeType::String, 'default' => 'bottom'],
+            'layout' => [AttributeType::String, 'default' => 'block'],
+            'palette' => [AttributeType::String, 'default' => 'default'],
+            'palette_banner' => [AttributeType::String, 'default' => '#000000'],
+            'palette_button' => [AttributeType::String, 'default' => '#f1d600'],
+            'palette_banner_text' => [AttributeType::String, 'default' => '#ffffff'],
+            'palette_button_text' => [AttributeType::String, 'default' => '#000000'],
+            'learn_more_link' => [AttributeType::String, 'default' => 'http://cookiesandyou.com/'],
+            'message' => [AttributeType::String, 'default' => 'This website uses cookies to ensure you get the best experience on our website.'],
+            'dismiss' => [AttributeType::String, 'default' => 'Got it!'],
+            'learn' => [AttributeType::String, 'default' => 'Learn More'],
 
-
-        );
+        ];
     }
 
+    /**
+     * @return mixed
+     */
     public function getSettingsHtml() {
-       return craft()->templates->render('craftcookieconsent/_settings', array(
+       return craft()->templates->render('craftcookieconsent/_settings', [
            'settings' => $this->getSettings()
-       ));
+       ]);
     }
 
     function init() {
@@ -64,13 +86,13 @@ class CraftCookieConsentPlugin extends BasePlugin
                             "text": "'. $settings->palette_banner_text .'"
                           },
                           "button": {
-                              "background":  "'. $settings->layout .'" == "wire" ? "transparent" :  "'. $settings->palette_button .'",
-                              "text": "'. $settings->layout .'" == "wire" ? "'. $settings->palette_button .'" : "'. $settings->palette_button_text .'",
-                              "border":  "'. $settings->layout .'" == "wire" ? "'. $settings->palette_button .'" : undefined
+                              "background":  "'. $settings->layout .'" === "wire" ? "transparent" :  "'. $settings->palette_button .'",
+                              "text": "'. $settings->layout .'" === "wire" ? "'. $settings->palette_button .'" : "'. $settings->palette_button_text .'",
+                              "border":  "'. $settings->layout .'" === "wire" ? "'. $settings->palette_button .'" : undefined
                           }
                       },
-                      "position": "'. $settings->position .'" == "toppush" ? "top" : "'. $settings->position .'",
-                      "static": "'. $settings->position .'" == "toppush",
+                      "position": "'. $settings->position .'" === "toppush" ? "top" : "'. $settings->position .'",
+                      "static": "'. $settings->position .'" === "toppush",
                       "theme": "'. $settings->layout .'",
                       "content": {
                           "message": "'. Craft::t($settings->message) .'",
